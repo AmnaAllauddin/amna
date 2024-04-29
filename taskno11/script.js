bbc5a3b82894873618d3d8420cba44703607ee8c
AmnaAllauddin <amnaallauddin2002@gmail.com>
2024-04-28 18:15:28 -0700

    28-04-2024

---
+++ taskno11/script.js
@@ -0,0 +1,35 @@
+// question 1
+var Name="amna";
+console.log( "question no 1 answer of print a string variable is :",Name);
+// question2
+var num1=1;
+var num2=45;
+var sum=num1+num2;
+console.log("question no 2 answer of sum is :",sum);
+// question3
+const PI=3.14159;
+var radius=5;
+var areaCircle=PI*radius*radius;
+console.log("question no 3 answer of areacircle is:",areaCircle);
+// question4
+var num=prompt("enter a number:")
+if(num>0)
+ console.log("question no 4 answer of  condition statement num is positive:",num) 
+else if(num<0)
+ console.log("question no 4 answer of   condition statement  num is negative:",num) 
+else if(num!=0)
+console.log(" question no 4 answer of  condition statement  not a number charachter:",num)
+else
+console.log("number is zero:",num)
+// question5
+var i=0;
+while(i<=10){
+    console.log("question no 5 answer of while loop is:",i)
+  i++;
+}
+// questionno6
+var i=0;
+for(i;i<=10;i++)
+console.log("question no 6 answer of for loop is:",i)
+
+
---
+++ taskno12/script.js
@@ -0,0 +1,20 @@
+// question no 1
+const numbers=[3,7,2,9,5];
+let max = numbers[0]; 
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log("maxium number from a array is:",max);
+// question no 2
+const number=[1,2,3,4,5,6];
+let sum=number[0];
+for(let i=0;i<=numbers.length;i+=1){
+        sum=sum+i;
+}
+console.log("sum of array is:",sum);
+
+ 
